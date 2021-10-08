66acd54f335fa07b397e00da299854c353e45a16
Brian Lee <briandl92391@gmail.com>
2021-10-07 17:42:10 -0700

    update ci

--- src/__tests__/tacto.test.ts
+++ src/__tests__/tacto.test.ts
@@ -1,53 +1,48 @@
-import tacto, { sorters, asc, desc } from "../";
+import tacto, { sorters, asc, desc } from '../'
 
-describe("Tacto", () => {
-    it("Should return shallow copies", () => {
-        const a = [4, 3, 3, 5, 9, 8]
-        expect(tacto<number>(sorters.asc(x => x))(a)).not.toBe(a);
-    })
+describe('Tacto', () => {
+  it('Should return shallow copies', () => {
+    const a = [4, 3, 3, 5, 9, 8]
+    expect(tacto<number>(sorters.asc((x) => x))(a)).not.toBe(a)
+  })
 
-    it("Should sort numbers", () => {
-       expect(tacto<number>(sorters.asc(x => x))([
-           4, 2, 3, 5, 9, 8
-        ])).toEqual([
-           2, 3, 4, 5, 8, 9
-       ]);
-    });
+  it('Should sort numbers', () => {
+    expect(tacto<number>(sorters.asc((x) => x))([4, 2, 3, 5, 9, 8])).toEqual([2, 3, 4, 5, 8, 9])
+  })
 
-    it("Can sort anything given a numeric value", () => {
-        expect(tacto<string>(sorters.desc(x => x.charCodeAt(0)))([
-            "x", "d", "e", "i", "b", "w"
-        ])).toEqual([
-            "x", "w", "i", "e", "d", "b"
-        ]);
-    });
+  it('Can sort anything given a numeric value', () => {
+    expect(tacto<string>(sorters.desc((x) => x.charCodeAt(0)))(['x', 'd', 'e', 'i', 'b', 'w'])).toEqual([
+      'x',
+      'w',
+      'i',
+      'e',
+      'd',
+      'b'
+    ])
+  })
 
-    it("Allows custom sort implementations", () => {
-        expect(tacto<string>(sorters.raw((a, b) => a.localeCompare(b)))([
-            "cat", "dog", "bird"
-        ])).toEqual([
-            "bird", "cat", "dog"
-        ]);
-    });
+  it('Allows custom sort implementations', () => {
+    expect(tacto<string>(sorters.raw((a, b) => a.localeCompare(b)))(['cat', 'dog', 'bird'])).toEqual([
+      'bird',
+      'cat',
+      'dog'
+    ])
+  })
 
-    it("Can perform compound sorts", () => {
-        const sort = tacto<string>(
-            // Sort by the first character ascending
-            sorters.asc(x => x.charCodeAt(0)),
-            // Then sort by the last character descending
-            sorters.desc(x => x.charCodeAt(x.length - 1))
-        );
-        expect(sort([
-            "xta", "xvb", "xwc", "aey", "axb", "aqh"
-        ])).toEqual([
-            "aey", "aqh", "axb", "xwc", "xvb", "xta"
-        ]);
-    });
+  it('Can perform compound sorts', () => {
+    const sort = tacto<string>(
+      // Sort by the first character ascending
+      sorters.asc((x) => x.charCodeAt(0)),
+      // Then sort by the last character descending
+      sorters.desc((x) => x.charCodeAt(x.length - 1))
+    )
+    expect(sort(['xta', 'xvb', 'xwc', 'aey', 'axb', 'aqh'])).toEqual(['aey', 'aqh', 'axb', 'xwc', 'xvb', 'xta'])
+  })
 
-    it("Provides convenience functions for simple ascending and descending sorts", () => {
-        expect(asc([10, 101, 10001])).toEqual([10, 101, 10001]);
-        expect(desc([10, 101, 10001])).toEqual([10001, 101, 10]);
-        expect(asc(["10", "101", "10001"])).toEqual(["10", "10001", "101"]);
-        expect(desc(["10", "101", "10001"])).toEqual(["101", "10001", "10"]);
-    })
-});
+  it('Provides convenience functions for simple ascending and descending sorts', () => {
+    expect(asc([10, 101, 10001])).toEqual([10, 101, 10001])
+    expect(desc([10, 101, 10001])).toEqual([10001, 101, 10])
+    expect(asc(['10', '101', '10001'])).toEqual(['10', '10001', '101'])
+    expect(desc(['10', '101', '10001'])).toEqual(['101', '10001', '10'])
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-type EvaluationResult = number|string;
+type EvaluationResult = number | string;
 type EvaluationFunction<T> = (x: T) => EvaluationResult;
+type CompareFunction<T> = (a: T, b: T) => number;
+type SortingFunction<T> = (sequence: T[]) => T[];
 
 type Sorter<T> =
   | {
@@ -11,44 +13,42 @@ type Sorter<T> =
       f: CompareFunction<T>;
     };
 
-type CompareFunction<T> = (a: T, b: T) => number;
-type SortingFunction<T> = (sequence: T[]) => T[];
-
 export const sorters = {
   asc: <T>(f: EvaluationFunction<T>): Sorter<T> => ({ type: 'asc', f }),
   desc: <T>(f: EvaluationFunction<T>): Sorter<T> => ({ type: 'desc', f }),
-  raw: <T>(f: CompareFunction<T>): Sorter<T> => ({ type: 'raw', f }),
-};
+  raw: <T>(f: CompareFunction<T>): Sorter<T> => ({ type: 'raw', f })
+}
 
 const tacto = <T>(...sorts: Array<Sorter<T>>): SortingFunction<T> => {
-  return items => [...items].sort((a: T, b: T): number => {
-    let result = 0;
+  return (items) =>
+    [...items].sort((a: T, b: T): number => {
+      let result = 0
 
-    for (const sort of sorts) {
-      if (sort.type === 'raw') {
-        result = sort.f(a, b);
-      } else {
-        const [v1, v2]: EvaluationResult[] = [sort.f(a), sort.f(b)];
-
-        if (typeof v1 === 'number' && typeof v2 === 'number') {
-          result = (sort.type === 'desc' ? -1 : 1) * (v1 - v2);
+      for (const sort of sorts) {
+        if (sort.type === 'raw') {
+          result = sort.f(a, b)
         } else {
-          result = (sort.type === 'desc' ? -1 : 1) * v1.toString().localeCompare(v2.toString())
+          const [v1, v2]: EvaluationResult[] = [sort.f(a), sort.f(b)]
+
+          if (typeof v1 === 'number' && typeof v2 === 'number') {
+            result = (sort.type === 'desc' ? -1 : 1) * (v1 - v2)
+          } else {
+            result = (sort.type === 'desc' ? -1 : 1) * v1.toString().localeCompare(v2.toString())
+          }
+        }
+
+        if (result !== 0) {
+          return result
         }
       }
 
-      if (result !== 0) {
-        return result;
-      }
-    }
-
-    return result;
-  });
-};
+      return result
+    })
+}
 
 tacto.sorters = sorters
 
-export const asc = tacto<EvaluationResult>(sorters.asc(x => x))
-export const desc = tacto<EvaluationResult>(sorters.desc(x => x))
+export const asc = tacto<EvaluationResult>(sorters.asc((x) => x))
+export const desc = tacto<EvaluationResult>(sorters.desc((x) => x))
 
-export default tacto;
+export default tacto
